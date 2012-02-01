63bbba626c30a74d5eb33152bc0291d4d925afdd
Eivind Uggedal <eivind@uggedal.com>
2012-02-01 10:59:08 +0100

    salt config

--- src/salt/bootstrap.sh
+++ src/salt/bootstrap.sh
@@ -6,3 +6,13 @@ apt-get install -qyt wheezy cython python-zmq python-yaml python-crypto \
   python-m2crypto python-jinja2 msgpack-python python-pip
 
 pip install salt
+
+cat > /etc/salt/master << 'EOF'
+interface: 127.0.0.1
+auto_accept: True
+file_roots:
+  base:
+    - /root/infrastruktur-som-kode/src/salt/state
+EOF
+
+echo "master: localhost" > /etc/salt/minion
