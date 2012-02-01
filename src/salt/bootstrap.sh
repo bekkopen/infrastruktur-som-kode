86803ced8e30a1c4c939dafa75c719ce5970fdc1
Eivind Uggedal <eivind@uggedal.com>
2012-02-01 10:14:33 +0100

    salt deps

--- src/salt/bootstrap.sh
+++ src/salt/bootstrap.sh
@@ -3,6 +3,6 @@
 set -e
 
 apt-get install -qyt wheezy cython python-zmq python-yaml python-crypto \
-  python-m2crypto python-jinja2 python-pip
+  python-m2crypto python-jinja2 msgpack-python python-pip
 
 pip install salt
