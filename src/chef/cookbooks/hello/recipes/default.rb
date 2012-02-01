30967ec14f1b78cc73120d3a82d0403dfb66657c
Eivind Uggedal <eivind@uggedal.com>
2012-02-01 16:19:08 +0100

    perm

--- src/chef/cookbooks/hello/recipes/default.rb
+++ src/chef/cookbooks/hello/recipes/default.rb
@@ -2,8 +2,11 @@ nginx_site "chef.uggedal.com" do
   root "/var/www"
 end
 
-directory "/var/www"
+directory "/var/www" do
+  mode "0755"
+end
 
 cookbook_file "/var/www/index.html" do
   source "index.html"
+  mode "0644"
 end
