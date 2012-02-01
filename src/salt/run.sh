8f5cff06fb1e08a380045e0f236601771b5f3e59
Eivind Uggedal <eivind@uggedal.com>
2012-02-01 11:03:21 +0100

    stderr

--- src/salt/run.sh
+++ src/salt/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pgrep salt-master > /dev/null || salt-master -d
-pgrep salt-minion > /dev/null || salt-minion -d
+pgrep salt-master > /dev/null 2>&1 || salt-master -d
+pgrep salt-minion > /dev/null 2>&1 || salt-minion -d
 
 salt '*' state.highstate
