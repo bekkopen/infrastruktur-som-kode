198e888de3d1817f69562e536dfc5364b64f8b94
Eivind Uggedal <eivind@uggedal.com>
2012-02-01 08:57:56 +0100

    Need to set default release when using testing repo.

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -6,6 +6,14 @@ mirror="http://ukdebian.mirror.anlx.net/debian/"
 
 echo "deb $mirror squeeze main contrib non-free" > /etc/apt/sources.list
 echo "deb $mirror wheezy main contrib non-free" > /etc/apt/sources.list.d/wheezy.list
+
+cat > /etc/apt/apt.conf << 'EOF'
+APT {
+  Default-Release "squeeze";
+  Install-Recommends "false";
+};
+EOF
+
 apt-get update
 apt-get dist-upgrade -y
 
