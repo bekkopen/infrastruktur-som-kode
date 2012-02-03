3fa04b84b6fce9ffcc99de89df2bd164044b8dc3
Eivind Uggedal <eivind@uggedal.com>
2012-02-03 16:09:40 +0100

    more

---
+++ src/salt/state/_states/nginx_site.py
@@ -0,0 +1,16 @@
+def present(name,
+            root=None,
+            source="salt://nginx/site.conf"
+            __env__='base'):
+
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+
+    site_available_name = "/etc/nginx/sites-available/%s.conf" % name,
+    context = dict(hostname=name, root=root)
+
+    site_available = __salt__['file.managed'](site_available_name,
+                                              source=source,
+                                              context=context,
+                                              __env__)
+
+    return site_available
